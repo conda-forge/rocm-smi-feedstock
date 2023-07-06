9b2894377fb217ff639d08258b7756ac097f1dac
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-07-06 16:49:33 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-
-# Library
 mkdir -p build
 cd build
 cmake ${CMAKE_ARGS} ..
 make
 make install 
 cd ..
-
-# Python
-mkdir -p $PREFIX/bin
-cp python_smi_tools/rocm_smi.py $PREFIX/bin/rocm_smi.py
-chmod +x $PREFIX/bin/rocm_smi.py
-ln -s $PREFIX/bin/rocm_smi.py $PREFIX/bin/rocm-smi
-cp python_smi_tools/rsmiBindings.py $SP_DIR/rsmiBindings.py
